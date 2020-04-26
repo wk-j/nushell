9ab0dd691f7741bb612bff97d5557f67e3da7be7
wk <somnuk.wk@gmail.com>
2020-04-26 20:08:19 +0700

    API

--- API.cmd
+++ API.cmd
@@ -1,5 +1,13 @@
 ## API
 
+Nu
+
 ```bash
 cat json/a1.json | http POST http://localhost:5000/api/authentication/login | from-json | get token
+```
+
+Jq
+
+```
+cat json/a1.json | http POST http://localhost:5000/api/authentication/login  | jq -r .token | xargs -I {} echo {} {}
 ```
